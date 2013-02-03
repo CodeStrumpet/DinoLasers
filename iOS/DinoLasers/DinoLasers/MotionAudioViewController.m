4c2192e7bbd33a0fb02a9163c8f5f92611825753
Julien Bloit <julien.bloit@gmail.com>
2013-02-03 15:32:55 -0800

    making sound

--- iOS/DinoLasers/DinoLasers/MotionAudioViewController.m
+++ iOS/DinoLasers/DinoLasers/MotionAudioViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "MotionAudioViewController.h"
+#import "PdAudioController.h"
+#import "PdBase.h"
 
 @interface MotionAudioViewController ()
+@property (nonatomic, strong) PdAudioController *audioController;
 
 @end
 
 @implementation MotionAudioViewController
+
+@synthesize audioController = audioController_;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -20,6 +25,11 @@
     if (self) {
         
         NSLog(@"Audio is Turned on");
+        self.audioController = [[PdAudioController alloc] init];
+        [self.audioController configurePlaybackWithSampleRate:44100 numberChannels:2 inputEnabled:YES mixingEnabled:NO];
+        [PdBase openFile:@"test.pd" path:[[NSBundle mainBundle] resourcePath]];
+        [self.audioController setActive:YES];
+        [self.audioController print];
         
     }
     return self;
